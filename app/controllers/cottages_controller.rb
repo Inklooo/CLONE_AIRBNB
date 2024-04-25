ae011f8b0d9a72c81fcb8238647bb29969ea5cd3
damien duport percier <damienviel69@gmail.com>
2024-04-25 19:44:38 -0400

    create collage controller

---
+++ app/controllers/cottages_controller.rb
@@ -0,0 +1,20 @@
+class CottagesController < ApplicationController
+  def new
+    @cottage = Cottage.new
+  end
+
+  def create
+    @cottage = Cottage.new(cottage_params)
+    if @cottage.save
+      redirect_to cottages_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def cottage_params
+    params.require(:cottage).permit(:name, :address, :description, :price, :availability, :user_id)
+  end
+end
