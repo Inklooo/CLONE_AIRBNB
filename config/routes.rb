9d4560194420c76116ea9027e3c45f91c2959cab
damien duport percier <damienviel69@gmail.com>
2024-04-27 14:59:07 -0400

    create controller bookings and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :cottages
+  resources :cottages do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :bookings, only: [:index, :show, :destroy]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-
-  resources :cottages
   # Defines the root path route ("/")
   # root "posts#index"
 end
