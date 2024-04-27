8c4023e1471d3a46400259a39c626bfd164611aa
damien duport percier <damienviel69@gmail.com>
2024-04-27 15:10:32 -0400

    coding bookingscontrollers methods

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,9 @@
 class BookingsController < ApplicationController
+  skip_before_action :authenticate_user!
+  before_action :set_booking, only: [:show, :destroy]
 
   def index
-
+    @bookings = Booking.all
   end
 
   def show
@@ -9,13 +11,29 @@ class BookingsController < ApplicationController
   end
 
   def new
-
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new, flash[:alert] = "Data error !"
+    end
   end
 
   def destroy
 
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :status)
   end
 end
