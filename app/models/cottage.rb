bdc95be0e933c84b2149e914a1ac312292ebab6b
damien duport percier <damienviel69@gmail.com>
2024-04-27 14:24:23 -0400

    remove availability from form

--- app/models/cottage.rb
+++ app/models/cottage.rb
@@ -2,4 +2,12 @@ class Cottage < ApplicationRecord
   has_many :bookings, dependent: :destroy
   belongs_to :user
   validates :name, :address, :description, :price, :availability, presence: true
+
+  before_validation :set_default_availability, on: :create
+
+  private
+
+  def set_default_availability
+    self.availability ||= true
+  end
 end
