d2c0afe75f662c2d34789f1651aab615e312067c
damien duport percier <damienviel69@gmail.com>
2024-04-30 19:59:28 -0400

    add-dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,10 @@ class PagesController < ApplicationController
   def home
   end
 
+  def dashboard
+    @user_bookings = current_user.bookings
+    @user_cottages = current_user.cottages
+    @cottages = current_user.cottages.includes(:bookings)
+  end
+
 end
