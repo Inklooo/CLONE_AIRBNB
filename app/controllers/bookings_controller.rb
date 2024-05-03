761e6454b869e9e8cb1da7dd0ae4cbb0a2abd48c
Arthur Ribe <arthur-ribe@hotmail.fr>
2024-05-02 20:21:15 -0400

    booking_save_changed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -38,8 +38,7 @@ class BookingsController < ApplicationController
     if @booking.save
       redirect_to dashboard_path
     else
-      flash[:alert] = "Data error !"
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
