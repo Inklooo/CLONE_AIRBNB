dcd432c712a9436183b9b348fb1ed94200534b47
Arthur Ribe <arthur-ribe@hotmail.fr>
2024-05-07 20:31:57 -0400

    map searchbar and map style

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -14,7 +14,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/arribe/clvx2yuw006qe01pe5myc39u4"
     })
     this.#addMarkersToMap();
     this.#fitMapToMarkers();
